0d28d6f98b131e1d3f247c12ea9f671abfe92835
Maciej Bryja <maciej.bryja@wp.pl>
2018-08-21 14:57:49 +0200

    addTest

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,5 +1,39 @@
 package service;
 
-public class UserServiceTest {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mac.bry.kurs1024kb.entity.User;
+import com.mac.bry.kurs1024kb.service.UserServiceImpl;
+
+public class UserServiceTest {
+	
+	@Test
+	public void testGetAllUsers() {
+		//is
+		List<User> users = new ArrayList<User>();
+		users.add(new User(1, "admin", "admin"));
+		users.add(new User(2, "adm", "adm"));
+		
+		//then
+		UserServiceImpl userServiceImpl = new UserServiceImpl(users);
+		List<User> usersFromTestClass = userServiceImpl.getAllUsers();
+		
+		//expected
+		Assert.assertEquals(users, usersFromTestClass);
+	}
+	
+	@Test
+	public void testAddUser() {
+		List<User> users = new ArrayList<User>();
+		User user = new User(12, "ADMIN", "ADMIN");
+		users.add(user);
+		UserServiceImpl userServiceImpl = new UserServiceImpl();
+		userServiceImpl.addUser(user);
+		List <User> usersFromTestClass = 
+		Assert.
+	}
 }
