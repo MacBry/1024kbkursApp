97c7f95b8d6c3de04c6e7d10e2bdde16249cc076
Maciej Bryja <maciej.bryja@wp.pl>
2018-09-06 21:11:12 +0200

    start work with UserValidator

--- src/main/java/com/mac/bry/kurs1024kb/validators/UserValidator.java
+++ src/main/java/com/mac/bry/kurs1024kb/validators/UserValidator.java
@@ -1,5 +1,27 @@
 package com.mac.bry.kurs1024kb.validators;
 
+import javax.management.InstanceAlreadyExistsException;
+
+import com.mac.bry.kurs1024kb.entity.User;
+
 public class UserValidator {
 
+	private static UserValidator instance = null;
+	
+	private UserValidator() {
+		
+	}
+	
+	public static UserValidator getInstance() {
+		if(instance == null) {
+			return new UserValidator();
+		}
+		return instance;
+	}
+	
+	public boolean isValidate(User user) {
+		return false;
+	}
+	
+	
 }
